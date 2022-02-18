cd77aee829c1223489e57c9f80e555eabd85e03a
Jaidatt <92085242+Jaidatt@users.noreply.github.com>
2022-02-18 19:14:46 +0530

    Add files via upload

---
+++ UploadTest.py
@@ -0,0 +1,86 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
+import numpy
+from keras.models import load_model
+from keras.preprocessing import image
+from tensorflow import keras
+
+model = load_model('model1.h5')
+# dictionary to label all traffic signs class.
+classes = ['Abra', 'Aerodactyl', 'Alakazam', 'Alolan Sandslash', 'Arbok', 'Arcanine', 'Articuno', 'Beedrill',
+                   'Bellsprout', 'Blastoise', 'Bulbasaur', 'Butterfree', 'Caterpie', 'Chansey', 'Charizard',
+                   'Charmander', 'Charmeleon', 'Clefable', 'Clefairy', 'Cloyster', 'Cubone', 'Dewgong', 'Diglett',
+                   'Ditto', 'Dodrio', 'Doduo', 'Dragonair', 'Dragonite', 'Dratini', 'Drowzee', 'Dugtrio', 'Eevee',
+                   'Ekans', 'Electabuzz', 'Electrode', 'Exeggcute', 'Exeggutor', 'Farfetchd', 'Fearow', 'Flareon',
+                   'Gastly', 'Gengar', 'Geodude', 'Gloom', 'Golbat', 'Goldeen', 'Golduck', 'Golem', 'Graveler',
+                   'Grimer', 'Growlithe', 'Gyarados', 'Haunter', 'Hitmonchan', 'Hitmonlee', 'Horsea', 'Hypno',
+                   'Ivysaur', 'Jigglypuff', 'Jolteon', 'Jynx', 'Kabuto', 'Kabutops', 'Kadabra', 'Kakuna', 'Kangaskhan',
+                   'Kingler', 'Koffing', 'Krabby', 'Lapras', 'Lickitung', 'Machamp', 'Machoke', 'Machop', 'Magikarp',
+                   'Magmar', 'Magnemite', 'Magneton', 'Mankey', 'Marowak', 'Meowth', 'Metapod', 'Mew', 'Mewtwo',
+                   'Moltres', 'MrMime', 'Muk', 'Nidoking', 'Nidoqueen', 'Nidorina', 'Nidorino', 'Ninetales', 'Oddish',
+                   'Omanyte', 'Omastar', 'Onix', 'Paras', 'Parasect', 'Persian', 'Pidgeot', 'Pidgeotto', 'Pidgey',
+                   'Pikachu', 'Pinsir', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Ponyta', 'Porygon', 'Primeape', 'Psyduck',
+                   'Raichu', 'Rapidash', 'Raticate', 'Rattata', 'Rhydon', 'Rhyhorn', 'Sandshrew', 'Sandslash',
+                   'Scyther', 'Seadra', 'Seaking', 'Seel', 'Shellder', 'Slowbro', 'Slowpoke', 'Snorlax', 'Spearow',
+                   'Squirtle', 'Starmie', 'Staryu', 'Tangela', 'Tauros', 'Tentacool', 'Tentacruel', 'Vaporeon',
+                   'Venomoth', 'Venonat', 'Venusaur', 'Victreebel', 'Vileplume', 'Voltorb', 'Vulpix', 'Wartortle',
+                   'Weedle', 'Weepinbell', 'Weezing', 'Wigglytuff', 'Zapdos', 'Zubat']
+
+
+# initialise GUI
+top = tk.Tk()
+top.geometry('800x600')
+top.title('Pokemon Classification')
+top.configure(background='#CDCDCD')
+label = Label(top, background='#CDCDCD', font=('arial', 15, 'bold'))
+sign_image = Label(top)
+
+
+def classify(file_path):
+    global classes
+    img = image.load_img(file_path, target_size=(256, 256))
+    x = image.img_to_array(img) / 255
+    resized_img_np = numpy.expand_dims(x, axis=0)
+    images = numpy.vstack([resized_img_np])
+    pred = model.predict(images)
+    x = numpy.argmax(pred)
+    sign = classes[x]
+
+    label.configure(foreground='#011638', text=sign)
+
+
+def show_classify_button(file_path):
+    classify_b = Button(top, text="Classify Pokemon",
+                        command=lambda: classify(file_path),
+                        padx=10, pady=5)
+    classify_b.configure(background='#364156', foreground='white',
+                         font=('arial', 10, 'bold'))
+    classify_b.place(relx=0.79, rely=0.46)
+
+
+def upload_image():
+    try:
+        file_path = filedialog.askopenfilename()
+        uploaded = Image.open(file_path)
+        uploaded.thumbnail(((top.winfo_width() / 2.25),
+                            (top.winfo_height() / 2.25)))
+        im = ImageTk.PhotoImage(uploaded)
+        sign_image.configure(image=im)
+        sign_image.image = im
+        label.configure(text='')
+        show_classify_button(file_path)
+    except:
+        pass
+
+
+upload = Button(top, text="Upload an image", command=upload_image, padx=10, pady=5)
+upload.configure(background='#364156', foreground='white', font=('arial', 10, 'bold'))
+upload.pack(side=BOTTOM, pady=50)
+sign_image.pack(side=BOTTOM, expand=True)
+label.pack(side=BOTTOM, expand=True)
+heading = Label(top, text="Pokemon Classification", pady=20, font=('arial', 20, 'bold'))
+heading.configure(background='#CDCDCD', foreground='#364156')
+heading.pack()
+top.mainloop()
